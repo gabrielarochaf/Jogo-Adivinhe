03199654945424f37125466850f01121ed90a09c
gabrielarochaf <oliveirgabriela278@gmail.com>
2026-05-22 10:44:47 -0300

    Build Header

--- jogo-adivinhacao/src/App.tsx
+++ jogo-adivinhacao/src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/Header";
+import styles from "./app.module.css";
+
 export default function App() {
-  return(
-    <div>
-      <h1>Hello world</h1>
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header />
+      </main>
     </div>
-  )
+  );
 }
